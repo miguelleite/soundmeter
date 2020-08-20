9bab3eeb1948e76fe8acdd3bb673786c4ee3951c
Miguel Leite <miguelacleite@gmail.com>
2020-08-20 23:24:27 +0100

    feat: Serve frontend from backend root path

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { environment } from '../environments/environment';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const productionModules = environment.production
+  ? [
+      ServeStaticModule.forRoot({
+        rootPath: join(__dirname, '..', 'client'),
+      }),
+    ]
+  : [];
+
 @Module({
-  imports: [],
+  imports: [...productionModules],
   controllers: [AppController],
   providers: [AppService],
 })
