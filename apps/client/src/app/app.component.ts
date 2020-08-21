bee4d3adf3de2b65af51989442b41789a0d4625d
Miguel Leite <miguelacleite@gmail.com>
2020-08-21 01:09:48 +0100

    feat: Organize imports

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { Message } from '@soundmeter/api-interfaces';
 
 @Component({
